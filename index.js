27388781460a457ef6850c495501af4d4a1741a3
SAhmed28 <sanjida1@ualberta.ca>
2022-12-30 08:34:21 -0600

    final push

--- index.js
+++ index.js
@@ -35,15 +35,6 @@ async function run(){
             res.send(users);
         });
 
-        // get user
-        // app.get('/users/role/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const query = { email: email }
-        //     const user = await usersCollection.findOne(query);
-        //     console.log(user);
-        //     res.send(user);
-        // })
-
 
 
         // get task information and save it in database
@@ -109,21 +100,7 @@ async function run(){
         });
 
 
-        // update task info
-        // app.patch('/taskEdit', async(req,res) => {
-        //     const task = req.body;
-        //     const id = task.id;
-        //     console.log(task);
-        //     const query = { _id: ObjectId(id) }
-        //     // const options = {upsert: true};
-        //     // const updatedDoc = {
-        //     //     $set: {
-        //     //         task : task
-        //     //     }
-        //     // }
-        //     // const result = await taskCollection.updateOne(query, updatedDoc);
-        //     // res.send(result);
-        // });
+       
 
         // task delete
         app.delete('/task/:id', async(req,res) => {
@@ -133,86 +110,6 @@ async function run(){
             res.send(result)
         })
 
-
-
-
-        // app.post('/bookings', async(req,res) => {
-        //     const booking = req.body;
-
-        //     const query = {
-        //         // email: booking.email,
-        //         productName: booking.productName,
-        //         productId: booking.productId 
-        //     }
-
-        //     const alreadyBooked = await bookingsCollection.find(query).toArray();
-
-        //     if (alreadyBooked.length){
-        //         const message = `${booking.productName} is already booked!`
-        //         return res.send({acknowledged: false, message})
-        //     }
-                
-        //     const result = await bookingsCollection.insertOne(booking);
-        //     return res.send(result);
-        // })
-
-
-        // app.get('/category/:brand', async (req,res)=> {
-        //     const brand = req.params.brand;
-        //     // console.log(brand);
-        //     const query = {productBrand: brand};
-        //     const categoryProducts = await productsCollection.find(query).toArray();
-        //     // console.log(categoryProducts);
-        //     res.send(categoryProducts);
-        // });
-
-
-        // app.get('/products', async(req,res) => {
-        //     let query = {};
-        //     if(req.query.email){
-        //         query = {
-        //             email: req.query.email
-        //         }
-        //     }
-        //     else if(req.query.isAdvertise){
-        //         query = {
-        //             isAdvertise: req.query.isAdvertise
-        //         }
-        //         // console.log(query)
-
-        //     }
-        //     const products = await productsCollection.find(query).toArray();
-        //     res.send(products);
-        // })
-
-        // app.post('/products', async (req,res) => {
-        //     const product = req.body;
-        //     const result = await productsCollection.insertOne(product);
-        //     res.send(result);
-        // });
-
-        // // patch  because I want to update it only if it is existing
-        // app.patch('/products/:id', async(req,res) => {
-        //     const id = req.params.id;
-        //     // console.log(id);
-        //     const query = { _id: ObjectId(id) }
-        //     const options = {upsert: true};
-        //     const updatedDoc = {
-        //         $set: {
-        //             isAdvertise : 'true'
-        //         }
-        //     }
-        //     const result = await productsCollection.updateOne(query, updatedDoc, options);
-        //     res.send(result);
-        // })
-
-        // // product delete
-        // app.delete('/products/:id', async(req,res) => {
-        //     const id = req.params.id;
-        //     const filter = {_id : ObjectId(id) };
-        //     const result = await productsCollection.deleteOne(filter);
-        //     res.send(result)
-        // })
     }
     finally{
 
